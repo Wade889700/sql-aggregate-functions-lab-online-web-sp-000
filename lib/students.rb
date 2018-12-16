14622f43c574435194b4586e808de3275eb56b16
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-16 00:27:32 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,8 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+# Language: Ruby, Level: Level 3
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  
+  SELECT 
 end
 
 def lowest_student_gpa
